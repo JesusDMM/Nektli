65a7e0baf195781bc512ef37ab267f18e0545d79
Chucho <119381852+JesusDMM@users.noreply.github.com>
2023-09-06 19:55:28 -0500

    Se arreglaron erores con las librerias

--- src/nektli/JLogin.java
+++ src/nektli/JLogin.java
@@ -95,11 +95,11 @@ public class JLogin extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-        // TODO add your handling code here:
+        //correo
     }//GEN-LAST:event_jTextField1ActionPerformed
 
     private void jPasswordField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordField1ActionPerformed
-        // TODO add your handling code here:
+        //contraseña
     }//GEN-LAST:event_jPasswordField1ActionPerformed
 
     /**
